431130fb1b1489ce8ea74c848683996cb869cd76
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-01-07 16:03:54 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -6,7 +6,7 @@ git add README.md README.pdf
 git commit -m "README"
 git add index.html mermain.css
 git commit -m "index"
-git add index.html INTERVAL.md README.md idmap.do wes.R weswgs.R weswgs.sh wgs.wrap
+git add SCALLOP-Seq.svg INTERVAL.md README.md idmap.do wes.R weswgs.R weswgs.sh wgs.wrap
 git add bgen.sb spa.* prune.wrap rva.*
 git commit -m "SCALLOP-seq association analysis"
 git add st.sh
