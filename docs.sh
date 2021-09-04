1cabdb76822fdbf1a56e29bc1d0a6d736c0fcb5d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-09-04 11:28:37 +0100

    docs.sh

--- docs.sh
+++ docs.sh
@@ -15,7 +15,7 @@ git commit -m ".gitignore"
 git add README.md
 git commit -m "README"
 git add SCALLOP-Seq.svg INTERVAL.md README.md idmap.do wes.R weswgs.R weswgs.sh wgs.wrap
-git add bgen.sb spa.* prune.wrap rva.*
+git add bgen.sb spa.* prune.wrap rva.* geneset_data/config.txt
 git commit -m "SCALLOP-seq association analysis"
 git add docs.sh
 git commit -m "docs.sh"
