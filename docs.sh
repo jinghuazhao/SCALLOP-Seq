ff88ff79c6eed1460cadc3e8a9df0e6f20e067c8
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-09-05 17:57:06 +0100

    docs

--- docs.sh
+++ docs.sh
@@ -19,8 +19,8 @@ git add bgen.sb spa.* prune.wrap rva.* geneset_data/config.txt
 git commit -m "SCALLOP-seq association analysis"
 git add docs docs.sh
 git commit -m "docs"
-git add test
-git commit -m test
+git add tests
+git commit -m tests
 git add mkdocs.yml
 git commit -m "mkdocs.yml"
 git push
