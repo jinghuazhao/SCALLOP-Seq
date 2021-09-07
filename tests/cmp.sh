ef179bce39b55fbaddbfe30d9cedc5b2f7abe182
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-09-07 20:03:20 +0000

    Deployed 81d4503 with MkDocs version: 1.1.2

--- tests/cmp.sh
+++ tests/cmp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-export flagship=~/rds/rds-jmmh2-results/private/interval_flagship/raw_results
-export scallop_seq=~/rds/projects/olink_proteomics/scallop/SCALLOP-Seq/work/spa
+export flagship=~/rds/results/private/interval_flagship/raw_results
+export spa=~/rds/projects/olink_proteomics/scallop/SCALLOP-Seq/work/spa
