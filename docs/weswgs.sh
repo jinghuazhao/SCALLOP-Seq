393418b82ea1118536f5facdec60485d1049c842
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-07-03 11:48:49 +0100

    docs

--- docs/weswgs.sh
+++ docs/weswgs.sh
@@ -2,4 +2,4 @@
 
 export PERL5LIB=
 ls | grep -e 'wes-' -e 'wgs-' | parallel -C' ' 'ls {}*' | gzip -f > weswgs.txt.gz
-gunzip -c weswgs.txt.gz | parallel -j8 -C' ' 'md5sum {}*' > weswgs.md5
+gunzip -c weswgs.txt.gz | parallel -j8 -C' ' 'md5sum {}*' | gzip -f > weswgs.md5.gz
