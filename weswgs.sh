a57e9bf2003aa4644ce6435039558bbf3136c2f7
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-07-03 11:32:46 +0100

    Deployed e816975 with MkDocs version: 1.5.3

---
+++ weswgs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+export PERL5LIB=
+ls | grep -e 'wes-' -e 'wgs-' | parallel -C' ' 'ls {}*' | gzip -f > weswgs.txt.gz
+gunzip -c weswgs.txt.gz | parallel -j8 -C' ' 'md5sum {}*' > weswgs.md5
