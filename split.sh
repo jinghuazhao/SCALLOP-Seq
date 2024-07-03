c1dc946a7cb76a8be3395af838524e0bd8ec8845
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-07-03 11:59:41 +0100

    Deployed 8d7bd5a with MkDocs version: 1.5.3

--- split.sh
+++ split.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+# files uploaded to GitHub
 export md5=~/rds/results/private/proteomics/scallop-seq/weswgs.md5.gz
 gunzip -c ${md5} | grep wes | sort -k2,2 | gzip -f > wes.md5.gz
 gunzip -c ${md5} | grep wgs | sort -k2,2 | gzip -f > wgs.md5.gz
@@ -7,4 +8,5 @@ export N=$(($(gunzip -c wgs.md5.gz | wc -l)/2+1))
 gunzip < wgs.md5.gz | split - --lines=$N --numeric-suffixes --suffix-length=1 wgs-
 gzip wgs-*
 rm wgs.md5.gz
+# regenderate locally from GitHub clone
 zcat wgs-{0..1}.gz | gzip -f > wgs.m5.gz
